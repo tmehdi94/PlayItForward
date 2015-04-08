493a2cf29b5e5e97360ec45b58df75fbbceffc8c
TAHA MEHDI <mehdit@rpi.edu>
2015-04-08 14:36:13 -0400

    change title of overview

--- newnew/overview.php
+++ newnew/overview.php
@@ -11,7 +11,7 @@ $loggedin = login_check($db);
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Play It Forward | Missions</title>
+<title>Play It Forward | Overview</title>
 <!-- Bootstrap -->
 <link href="css/bootstrap.css" rel='stylesheet' type='text/css'/>
 <!-- Font Awesome  -->
