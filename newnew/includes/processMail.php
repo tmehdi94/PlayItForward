e93ab30ff160ffac27af321a2e1d251cd2fab1d4
rhodyj <rhodyjake@gmail.com>
2015-04-29 14:59:03 -0400

    Minor fixes
    e-mail still doesn't work

--- newnew/includes/processMail.php
+++ newnew/includes/processMail.php
@@ -10,11 +10,12 @@ if (isset($_POST['name'], $_POST['address'], $_POST['subject'], $_POST['message'
     $sender = $_POST['address'];
     $subjectline = $_POST['subject'];
     $message = $_POST['message'];
-    echo $name.$sender."D:LFKJS:DLFKJ".$message;
+    //$headers = 'From: PlayitForward.ThugLife@gmail.com';
+
     //Should be implemented when a valid domain is selected
-    if($name != 'Name' && $sender != 'Email Address' && $subjectline != 'Subject' && $message != "Message"){
+    if($name != 'Name' && $sender != 'Email Address' && $subjectline != 'Subject' && $message != 'Message'){
         mail($sender, "Copy of message: ".$subjectline." | DO NOT RESPOND", $message); //sends a copy to desired e-mail. Will also check if e-mail entered is valid
-        mail("playitforward.thuglife@gmail.com", $subjectline, $message."\n\n-".$name);
+        mail("PlayitForward.ThugLife@gmail.com", $subjectline, $message."\n\n-".$name."\t".$sender);
         header('Location: ../contact.php?success=1');
     }else{
     header('Location: ../contact.php?error=1');
